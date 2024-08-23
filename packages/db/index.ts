7b4b4cf7db7eeecd2b448bc21f4344efa773ae74
jacky <xtcelzhangyong@126.com>
2024-08-23 11:16:31 +0800

    change database to mysql

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,16 +1,26 @@
 import type { DB } from "./prisma/types";
-import { Pool } from 'pg'
-import { Kysely, PostgresDialect } from "kysely"
+import { Kysely } from "kysely"
+import { PlanetScaleDialect } from "kysely-planetscale";
+import { env } from "./env.mjs";
 
-export { jsonArrayFrom, jsonObjectFrom } from "kysely/helpers/postgres";
+// export { jsonArrayFrom, jsonObjectFrom } from "kysely/helpers/postgres";
 
 export * from "./prisma/types";
 export * from "./prisma/enums";
 
+// MySQL
 export const db = new Kysely<DB>({
-  dialect: new PostgresDialect({
-    pool: new Pool({
-      connectionString: process.env.DATABASE_URL,
+  dialect: new PlanetScaleDialect({
+      url: env.MYSQL_URL,
     }),
-  }),
-});
+  });
+
+// PostgreSQL
+// export const db = new Kysely<DB>({
+//   dialect: new PostgresDialect({
+//     pool: new Pool({
+//       connectionString: process.env.DATABASE_URL,
+//     }),
+//   }),
+// });
+
