541fc8a6ace7b3deb484b7f165556cd40e655f4e
jacky <xtcelzhangyong@126.com>
2024-08-23 10:51:55 +0800

    fix VercelPostgresError - 'invalid_connection_string' when using createPool bug

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,10 +1,16 @@
-import { createKysely } from "@vercel/postgres-kysely";
-
 import type { DB } from "./prisma/types";
+import { Pool } from 'pg'
+import { Kysely, PostgresDialect } from "kysely"
 
 export { jsonArrayFrom, jsonObjectFrom } from "kysely/helpers/postgres";
 
 export * from "./prisma/types";
 export * from "./prisma/enums";
 
-export const db = createKysely<DB>();
+export const db = new Kysely<DB>({
+  dialect: new PostgresDialect({
+    pool: new Pool({
+      connectionString: process.env.DATABASE_URL,
+    }),
+  }),
+});
